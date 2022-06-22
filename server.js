8f8c39a61e32e5ce0c92fdec9784dd190870b9b5
Himansumallick <dreamhimansu@gmail.com>
2022-06-22 08:37:13 +0545

    html completed

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const flapApp = express()
 const fs = require('fs')
+const index = fs.readFileSync('./public/index.html',{encoding:'utf8'})
 
-
-
+flapApp.use(express.static('public'));
+flapApp.get('/',(req,res)=>{
+    res.setHeader("Content-Type", "text/html")
+    res.status(200).send(index)
+})
 flapApp.listen(4000, () => {
     console.log('Server running on port 4000...')
 })
